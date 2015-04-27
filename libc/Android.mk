13c4ecf6f61b04c113293c67db8538068849a983
Kyle Repinski <repinski23@gmail.com>
2015-04-27 15:36:58 -0500

    libc: Don't build malloc leak check for userdebug.
    Change-Id: I067fed514cf427d4b4de8c29586f3452b5af2aec

--- libc/Android.mk
+++ libc/Android.mk
@@ -1122,7 +1122,7 @@ LOCAL_STATIC_LIBRARIES_arm := libc++
 LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 
 # Don't install on release build
-LOCAL_MODULE_TAGS := eng debug
+LOCAL_MODULE_TAGS := eng
 
 $(eval $(call patch-up-arch-specific-flags,LOCAL_CFLAGS,libc_common_cflags))
 include $(BUILD_SHARED_LIBRARY)
@@ -1154,7 +1154,7 @@ LOCAL_SHARED_LIBRARIES := libc libdl
 LOCAL_SYSTEM_SHARED_LIBRARIES :=
 
 # Don't install on release build
-LOCAL_MODULE_TAGS := eng debug
+LOCAL_MODULE_TAGS := eng
 
 $(eval $(call patch-up-arch-specific-flags,LOCAL_CFLAGS,libc_common_cflags))
 include $(BUILD_SHARED_LIBRARY)
